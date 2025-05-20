c75d4e53bfd72e18609acf4a35b2b7c3f04a1535
CitraSuryanto <citrasuryanto55@gmail.com>
2025-05-20 12:13:24 +0700

    update

---
+++ fasumpab2/fasumpab2/android/app/src/main/kotlin/com/example/fasumpab2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fasumpab2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ fasumpab2/fasumpab2/lib/screens/full_image_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class FullImageScreen extends StatelessWidget {
+  const FullImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
